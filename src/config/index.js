8f1d5830af62742a856d35bb3713d60438d3e2c3
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2019-01-11 17:02:05 -0500

    Added docker support

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+export const CONFIG = {
+	NODE_ENV: process.env.NODE_ENV || 'development',
+	SERVER_URL:
+		process.env.NODE_ENV === 'production'
+			? 'https://purduehackers.herokuapp.com'
+			: process.env.REACT_APP_SERVER_URL
+				? process.env.REACT_APP_SERVER_URL
+				: 'http://localhost:5000'
+};
+
+console.log('CONFIG:', CONFIG);
