d0db6e5692523149c77f30e37df893b27736697a
Pankaj <meghanipankaj@pme.me>
2021-02-15 00:17:19 -0500

    Removed Marker issue

--- src/components/Common/index.js
+++ src/components/Common/index.js
@@ -11,4 +11,3 @@ export { default as Footer } from './Footer';
 export { default as FlashMessage } from './FlashMessage';
 export { default as Navigation } from './Navbar';
 // export { default as Header } from './Header';
-export { default as MarkerInfo } from './MarkerInfo';
